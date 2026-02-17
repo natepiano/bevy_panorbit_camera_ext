a57558f8c2b8d157ac0b4d02c997ddb8ff06cd1b
natepiano <slicks.curable.0k@icloud.com>
2026-02-17 15:05:06 -0500

    formatting update

--- src/lib.rs
+++ src/lib.rs
@@ -16,19 +16,15 @@ mod zoom;
 // Public API - Completion events
 // Public API - Animation types
 // Internal - used by plugin, not for external use
-use animation::process_camera_move_list;
 pub use animation::CameraMove;
 pub use animation::CameraMoveList;
+use animation::process_camera_move_list;
 pub use events::AnimationBegin;
 pub use events::AnimationEnd;
 pub use events::CameraMoveBegin;
 pub use events::CameraMoveEnd;
 pub use events::ZoomBegin;
 pub use events::ZoomEnd;
-use extension::on_animate_to_fit;
-use extension::on_play_animation;
-use extension::on_set_fit_target;
-use extension::on_zoom_to_fit;
 pub use extension::AnimateToFit;
 // Public API - Traits
 pub use extension::PanOrbitCameraExt;
@@ -36,8 +32,12 @@ pub use extension::PlayAnimation;
 pub use extension::SetFitTarget;
 pub use extension::ZoomAnimationMarker;
 pub use extension::ZoomToFit;
-use smoothness::restore_smoothness_on_move_end;
+use extension::on_animate_to_fit;
+use extension::on_play_animation;
+use extension::on_set_fit_target;
+use extension::on_zoom_to_fit;
 pub use smoothness::SmoothnessStash;
+use smoothness::restore_smoothness_on_move_end;
 // Public API - Gizmo groups (for enabling/disabling)
 pub use visualization::FitTargetGizmo;
 pub use visualization::FitTargetMargins;
